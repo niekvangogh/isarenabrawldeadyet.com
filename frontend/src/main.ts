ed7cf6506946faeb2b765e2f3a5a28fd3600a5be
Niek <ik@niekvangogh.nl>
2020-04-26 01:45:39 +0200

    add backend

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+axios.defaults.baseURL = process.env.API_URL;
+Vue.use(VueAxios, axios)
+
 Vue.config.productionTip = false;
 
 new Vue({
---
+++ frontend/src/views/Index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component({})
+export default class Index extends Vue {
+  private mounted() {}
+}
+</script>
